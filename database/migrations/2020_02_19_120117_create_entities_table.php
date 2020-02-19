c25131d749b9449aa84971960969a5380c62bf48
Bronyrus <bronyruschannel@gmail.com>
2020-02-19 15:49:31 +0300

    Update 2020_02_19_120117_create_entities_table.php

--- database/migrations/2020_02_19_120117_create_entities_table.php
+++ database/migrations/2020_02_19_120117_create_entities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateObjectsTable extends Migration
+class CreateEntitiesTable extends Migration
 {
     /**
      * Run the migrations.
