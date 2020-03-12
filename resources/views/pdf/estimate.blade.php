29b70ddbc1cb2385c1adfcfa429de2a61f83848f
Bronyrus <bronyruschannel@gmail.com>
2020-03-12 15:01:03 +0300

    Update estimate.blade.php

--- resources/views/pdf/estimate.blade.php
+++ resources/views/pdf/estimate.blade.php
@@ -17,10 +17,10 @@
                 <tbody>
                   @foreach($estimate as $item)
                     <tr>
-                      <td>{{ $item->name }}</td>
-                      <td>{{ $item->count }}</td>
-                      <td>{{ $item->amount }}</td>
-                      <td>{{ $item->price }}</td>
+                      <td>{{ $item['name'] }}</td>
+                      <td>{{ $item['count'] }}</td>
+                      <td>{{ $item['amount'] }}</td>
+                      <td>{{ $item['price'] }}</td>
                     </tr>
                   @endforeach
                   <tr>
