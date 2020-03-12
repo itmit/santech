51fecd6211ec474fb8ffa9fa0375b83babc5f001
Bronyrus <bronyruschannel@gmail.com>
2020-03-12 15:31:55 +0300

    опа

--- resources/views/pdf/estimate.blade.php
+++ resources/views/pdf/estimate.blade.php
@@ -4,7 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <h1>{{ $entity->name }}</h1>
+            {{-- <h1>{{ $entity->name }}</h1> --}}
+            <h1>Наименование объекта</h1>
             <table>
                 <thead>
                   <tr>
@@ -15,19 +16,32 @@
                   </tr>
                 </thead>
                 <tbody>
-                  @foreach($estimate as $item)
+                  {{-- @foreach($estimate as $item)
                     <tr>
                       <td>{{ $item['name'] }}</td>
                       <td>{{ $item['count'] }}</td>
                       <td>{{ $item['amount'] }}</td>
                       <td>{{ $item['price'] }}</td>
                     </tr>
-                  @endforeach
+                  @endforeach --}}
+                  <tr>
+                    <td>Наименование</td>
+                    <td>Количество}</td>
+                    <td>Цена</td>
+                    <td>Итого</td>
+                  </tr>
+                  <tr>
+                    <td>Наименование</td>
+                    <td>Количество}</td>
+                    <td>Цена</td>
+                    <td>Итого</td>
+                  </tr>
                   <tr>
                     <td><b>Total</b></td>
                     <td></td>
                     <td></td>
-                    <td>{{ $total }}</td>
+                    {{-- <td>{{ $total }}</td> --}}
+                    <td>100500р/td>
                   </tr>
                 </tbody>     
             </table>         
