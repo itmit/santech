ffa89a2129913330528dc826dbabb56bf7cfbb0c
Bronyrus <bronyruschannel@gmail.com>
2020-03-13 09:53:34 +0300

    Update EntityApiController.php

--- app/Http/Controllers/Api/EntityApiController.php
+++ app/Http/Controllers/Api/EntityApiController.php
@@ -141,12 +141,10 @@ class EntityApiController extends ApiBaseController
             $estimate = $items;
         };
 
-        // return $this->sendResponse($estimate, "Object's nodes");
-
         $pdf = PDF::loadView('pdf.estimate', ['estimate' => $estimate, 'entity' => $entity, 'total' => $total]);
     
         $pdf->save(storage_path().'/app/public/estimate/'.$uuid.'.pdf');
-        $link = storage_path().'/app/public/estimate/'.$uuid.'.pdf';
+        $link = 'storage/app/public/estimate/'.$uuid.'.pdf';
 
         return $this->sendResponse([$link], "PDF");
     }
