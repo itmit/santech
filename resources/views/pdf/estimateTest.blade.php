35edbb506c94f4d4151d249afbe00982168d31b9
Bronyrus <bronyruschannel@gmail.com>
2020-03-30 11:46:55 +0300

    Update estimateTest.blade.php

--- resources/views/pdf/estimateTest.blade.php
+++ resources/views/pdf/estimateTest.blade.php
@@ -6,34 +6,36 @@
         <div class="col-md-12">
             <p style="text-align: left"><b>Наименование объекта</b></p>
             {{-- <h1>Наименование объекта</h1> --}}
-            <table class="table-bordered" style="width: 100%; border: 5px solid black;border-collapse: collapse;">
-                <thead>
-                  <tr>
-                    <th style="text-align: left">№ п/п</th>
-                    <th style="text-align: left">Наименование</th>
-                    <th style="text-align: right">Кол-во</th>
-                    {{-- <th style="text-align: right">Цена</th>
-                    <th style="text-align: right">Итого</th> --}}
-                  </tr>
-                </thead>
-                <tbody>
+            <div class="table-responsive">
+                <table class="table-bordered" style="width: 100%; border: 2px solid black;border-collapse: collapse;">
+                    <thead>
                     <tr>
-                      <td style="text-align: right">1</td>
-                      <td style="text-align: left">Штука 1</td>
-                      <td style="text-align: right">5</td>
+                        <th style="text-align: left">№ п/п</th>
+                        <th style="text-align: left">Наименование</th>
+                        <th style="text-align: right">Кол-во</th>
+                        {{-- <th style="text-align: right">Цена</th>
+                        <th style="text-align: right">Итого</th> --}}
                     </tr>
-                    <tr>
-                        <td style="text-align: right">2</td>
-                        <td style="text-align: left">Штука 2</td>
-                        <td style="text-align: right">10</td>
-                    </tr>
-                    <tr>
-                        <td style="text-align: right">3</td>
-                        <td style="text-align: left">Штука 3</td>
-                        <td style="text-align: right">15/td>
-                    </tr>
-                </tbody>     
-            </table>         
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td style="text-align: right">1</td>
+                        <td style="text-align: left">Штука 1</td>
+                        <td style="text-align: right">5</td>
+                        </tr>
+                        <tr>
+                            <td style="text-align: right">2</td>
+                            <td style="text-align: left">Штука 2</td>
+                            <td style="text-align: right">10</td>
+                        </tr>
+                        <tr>
+                            <td style="text-align: right">3</td>
+                            <td style="text-align: left">Штука 3</td>
+                            <td style="text-align: right">15</td>
+                        </tr>
+                    </tbody>     
+                </table> 
+            </div>        
         </div>
     </div>
 </div>
