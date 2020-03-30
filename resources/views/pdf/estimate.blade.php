5c97e2a4fe1baefe8eb4513e29eb7114d41743f1
Bronyrus <bronyruschannel@gmail.com>
2020-03-30 13:47:25 +0300

    Update estimate.blade.php

--- resources/views/pdf/estimate.blade.php
+++ resources/views/pdf/estimate.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.pdfApp')
 
 @section('content')
-<div class="container">
+<div class="container" style="width: 100%">
     {{-- <div class="row justify-content-center">
         <div class="col-md-12"> --}}
             <p style="text-align: left"><b>{{ $entity->name }}</b></p>
