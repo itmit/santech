ba7235aba60316ec964a594562545b85335ac224
Bronyrus <36816185+Bronyrus@users.noreply.github.com>
2020-04-08 11:43:23 +0300

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -157,7 +157,8 @@
         $(document).on('click', 'button[name="js-save-change"]', function() {
             let id = $(this).data('i');
             let name = $('input[name="item-name"][data-i="'+id+'"]').val();
-            let file = $('input[name="js-photo"][data-i="'+id+'"]').val();
+            let file = $('input[name="js-photo"][data-i="'+id+'"]').files[0];
+            let formData = new FormData();
             console.log(name + ' ' + file);
         })
 
